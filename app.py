b82b1c64325d48b51d4005e57ae770c5ef43d171
mckova <maysidon@gmail.com>
2025-05-17 12:55:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__, static_folder="static", static_url_path="/static")
 CORS(app)
 
 # Load model once at startup (GPU recommended)
-model = MusicGen.get_pretrained("small").cuda().eval()
+model = MusicGen.get_pretrained("small")
+model = model.to(torch.device("cuda")).eval()
 
 @app.route("/generate-audio", methods=["POST"])
 def generate_audio():
